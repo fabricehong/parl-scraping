6f7933eb43e984d406f994ac92a9e89e033461bb
Fabrice Hong <fabrice.hong@liip.ch>
2015-09-05 12:14:13 +0200

    set static port for docker container. added information after script start for accessing the container kibana and elastic search instances

--- start_env.sh
+++ start_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -P -d -v "$PWD/esdata":/usr/share/elasticsearch/data --name=elasticsearch elasticsearch
-docker run -P --name=kibana --link elasticsearch:elasticsearch -d kibana
+docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/esdata":/usr/share/elasticsearch/data --name=elasticsearch elasticsearch
+docker run -p 3335:5601 --name=kibana --link elasticsearch:elasticsearch -d kibana
+
+echo "You can now access elastic search from http://localhost:3333 and kibana from http://localhost:3335"
