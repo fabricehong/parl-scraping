6d40521c4f72bd508ef18496fd7f1d494cee300c
Fabrice Hong <fabrice.hong@liip.ch>
2015-09-05 13:48:52 +0200

    added config directory

--- start_env.sh
+++ start_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/esdata":/usr/share/elasticsearch/data --name=elasticsearch elasticsearch
+# for sharing index folder. Not usefull now ?
+#docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD/config":/usr/share/elasticsearch/config --name=elasticsearch elasticsearch
+docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/elasticsearch/config":/usr/share/elasticsearch/config --name=elasticsearch elasticsearch
 docker run -p 3335:5601 --name=kibana --link elasticsearch:elasticsearch -d kibana
 
 echo "You can now access elastic search from http://localhost:3333 and kibana from http://localhost:3335"
