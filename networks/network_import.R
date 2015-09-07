de51d122db86769094742a97c6fd8320930ab812
yrochat <yrochat@Yannicks-MacBook-Pro.local>
2015-09-07 09:46:26 +0200

    included graphs for each sessions

--- networks/network_import.R
+++ networks/network_import.R
@@ -21,36 +21,31 @@ arcs$date <- as.character(ymd(arcs$date))
 vertices <- read.csv("graph-full/nodes.csv", sep="&", header=FALSE, stringsAsFactors = FALSE)
 colnames(vertices) <- c("id", "surname", "name", "canton", "polgroup")
 
-# create graph
+# create graph with multiple arcs
 g.all <- graph.data.frame(arcs, vertices=vertices)
+
+# create simplified graph
 g <- g.all
 E(g)$weight <- 1
 g <- simplify(g)
-E(g)$weight
 
+# create graphs of each sessions (will reduce the number of nodes and allow comparisons)
 
+ef1995 <- new_interval(ymd("1996-01-01"), ymd("1999-12-31"))
+ef1999 <- new_interval(ymd("2000-01-01"), ymd("2003-12-31"))
+ef2003 <- new_interval(ymd("2004-01-01"), ymd("2007-12-31"))
+ef2007 <- new_interval(ymd("2008-01-01"), ymd("2011-12-31"))
+ef2011 <- new_interval(ymd("2012-01-01"), ymd("2015-12-31"))
 
-# ranks of edges/dialogues
-table(E(g)$weight)
+int <- c(ef1995, ef1999, ef2003, ef2007, ef2011)
 
+g1995 <- subgraph.edges(g.all, eids=E(g.all)[ymd(E(g.all)$date) %within% ef1995])
+g1999 <- subgraph.edges(g.all, eids=E(g.all)[ymd(E(g.all)$date) %within% ef1999])
+g2003 <- subgraph.edges(g.all, eids=E(g.all)[ymd(E(g.all)$date) %within% ef2003])
+g2007 <- subgraph.edges(g.all, eids=E(g.all)[ymd(E(g.all)$date) %within% ef2007])
+g2011 <- subgraph.edges(g.all, eids=E(g.all)[ymd(E(g.all)$date) %within% ef2011])
 
-# regexpr(V(g)$id)
-
-
-
-# plotting
-g$layout <- layout_with_kk(g)
-
-V(g)$color <- rainbow(length(levels(factor(V(g)$polgroup))))[factor(V(g)$polgroup)]
-V(g)$label <- paste(V(g)$surname, V(g)$name, sep=" ")
-V(g)$size <- 2
-
-E(g)$color <- "gray"
-E(g)[which(E(g)$weight == 1)]$color <- NA
-E(g)$width <- sqrt(E(g)$weight)
-E(g)$arrow.size <- .1
-
-pdf("network_dialogues.pdf", width = 50, height = 50)
-par(mar=c(0,0,0,0))
-plot(g)
-dev.off()
+# save network
+# g.all is the network with multiple arcs
+save(g, g.all, file="graph-full/networks-all.Rdata")
+save(g1995, g1999, g2003, g2007, g2011, file="graph-full/networks-sessions.Rdata")
