525d00f59cc93fbe4548dcffa7ac24f9f5912581
Fabrice <fabrice.hong@gmail.com>
2015-09-28 19:29:24 +0200

    added kibana custom modification to environement

--- start_env.sh
+++ start_env.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# for sharing index folder. Not usefull now ?
-# docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD/config":/usr/share/elasticsearch/config --name=elasticsearch elasticsearch
-docker run -p 3333:9200 -p 3334:9300 -d --name=elasticsearch elasticsearch
-# docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/elasticsearch/config":/usr/share/elasticsearch/config --name=elasticsearch elasticsearch
-docker run -p 3335:5601 --name=kibana --link elasticsearch:elasticsearch -d kibana
+#elastic search
+docker run -p 3333:9200 -p 3334:9300 -d -v "$PWD/elastic_share":/elastic_share --name=elasticsearch elasticsearch
+
+#kibana
+#build custom kibana image
+docker build -t parl_kibana ./docker/kibana_image
+#run par_kibana image
+docker run -p 3335:5601 --name=kibana --link elasticsearch:elasticsearch -d parl_kibana
+
+#more info:
+
+# to share a directory with the container, use -v DIR_HOST:DIR_CONTAINER  . Example : -v "$PWD/kibana_share":/kibana_share
+# to share elasticsearch config, use -v "$PWD/elasticsearch/config":/usr/share/elasticsearch/config
+# to share elasticsearch data, use -v "$PWD/esdata":/usr/share/elasticsearch/data
 
 echo "You can now access elastic search from http://localhost:3333 and kibana from http://localhost:3335"
